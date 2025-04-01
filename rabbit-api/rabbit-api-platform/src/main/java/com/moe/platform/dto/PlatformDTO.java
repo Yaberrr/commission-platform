3fb78475db2e074d95bd2f8cc1eddd9aaaab5f15
tangyabo <13316022418@163.com>
2025-04-01 10:56:36 +0800

    fix:补充实体类

---
+++ rabbit-api/rabbit-api-platform/src/main/java/com/moe/platform/dto/PlatformDTO.java
@@ -0,0 +1,16 @@
+package com.moe.platform.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+/**
+ * @author tangyabo
+ * @date 2025/3/31
+ */
+@Data
+@AllArgsConstructor
+public class PlatformDTO {
+
+    //平台类型
+    private Integer platformType;
+}
