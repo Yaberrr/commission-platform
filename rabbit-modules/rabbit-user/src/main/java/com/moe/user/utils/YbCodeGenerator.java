6917424df8ad52a76acc74f929e0f480bd8ea4eb
tangyabo <13316022418@163.com>
2025-04-09 17:50:05 +0800

    feat:元宝码生成器实现

---
+++ rabbit-modules/rabbit-user/src/main/java/com/moe/user/utils/YbCodeGenerator.java
@@ -0,0 +1,44 @@
+package com.moe.user.utils;
+
+import com.moe.common.core.constant.CacheConstants;
+import com.moe.user.config.BloomFilterConfig;
+import org.redisson.api.RBloomFilter;
+import org.redisson.api.RedissonClient;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+import java.util.Random;
+
+/**
+ * 元宝号生成器
+ * @author tangyabo
+ * @date 2025/4/9
+ */
+@Component
+public class YbCodeGenerator {
+
+    @Autowired
+    private RedissonClient redissonClient;
+
+    private static final String BITMAP_KEY = CacheConstants.YB_CODE_BIT_MAP_KEY;
+
+    private static final int MIN = 100000; // 最少6位
+
+    private static final int MAX = 999999999; // 最多9位
+
+    private final Random random = new Random();
+
+    public String get() {
+        RBloomFilter<Integer> ybCodeFilter = redissonClient.getBloomFilter(BloomFilterConfig.YB_CODE_FILTER);
+        int code;
+        do {
+            code = MIN + random.nextInt(MAX - MIN + 1);
+        }//检查布隆过滤器
+        while (ybCodeFilter.contains(code));
+
+        //刷新布隆过滤器
+        ybCodeFilter.add(code);
+        return Integer.toString(code);
+    }
+
+}
